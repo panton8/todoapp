d3cb9d6140adde65f0ec3143636af21dd48d084f
panton8 <podvalnikov0@gmail.com>
2023-10-20 02:25:44 +0300

    Add urls for login & registration

--- todoapp/tasks/urls.py
+++ todoapp/tasks/urls.py
@@ -3,8 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='list'),
-    path('update_task/<str:pk>/', views.updateTask, name='update_task'),
-    path('delete_task/<str:pk>/', views.deleteTask, name='delete_task'),
-    path('complete_task/<str:pk>/', views.completeTask, name='complete_task'),
-    path('day/', views.current_day_tasks, name='list_by_day')
+    path('update_task/<str:pk>/', views.update_task, name='update_task'),
+    path('delete_task/<str:pk>/', views.delete_task, name='delete_task'),
+    path('complete_task/<str:pk>/', views.complete_task, name='complete_task'),
+    path('day/', views.current_day_tasks, name='list_by_day'),
+    path('signin/', views.sign_in, name='sign_in'),
+    path('signout/', views.sign_out, name='sign_out'),
+    path('signup/', views.sign_up, name='sign_up'),
 ]
