4079aea43d419f4091bc9e129068c7b5affbdd9b
panton8 <podvalnikov0@gmail.com>
2023-10-19 15:36:31 +0300

    Add new urls

--- todoapp/tasks/urls.py
+++ todoapp/tasks/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index, name='list'),
+    path('update_task/<str:pk>/', views.updateTask, name='update_task'),
+    path('delete_task/<str:pk>/', views.deleteTask, name='delete_task'),
+    path('complete_task/<str:pk>/', views.completeTask, name='complete_task'),
+    path('day/', views.current_day_tasks, name='list_by_day')
 ]
